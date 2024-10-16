32ff3338ad3557379bd365764055911502f71a6a
Alexey Erhskov <ershkov.alex2000@gmail.com>
2024-10-16 16:41:03 +0300

    fix undefined filter

--- packages/postcss-logical-fallback/CHANGELOG.md
+++ packages/postcss-logical-fallback/CHANGELOG.md
@@ -1,6 +1,10 @@
 # Change Log - postcss-logical-fallback
 
-This log was last generated on Fri, 15 Dec 2023 09:08:20 GMT and should not be manually modified.
+## 1.0.3
+Fix can't read filter of undefined
+
+## 1.0.2
+Fix comments empty rules generation bug
 
 ## 1.0.1
 Fri, 15 Dec 2023 09:08:20 GMT
--- packages/postcss-logical-fallback/src/utils/has-empty-child-nodes.ts
+++ packages/postcss-logical-fallback/src/utils/has-empty-child-nodes.ts
@@ -1,6 +1,6 @@
 import { Container } from 'postcss';
 
-export const hasEmptyChildNodes = ({ nodes }: Container) => {
+export const hasEmptyChildNodes = ({ nodes = [] }: Container) => {
     const significantNodes = nodes.filter((node) => node.type !== 'comment');
-    return !significantNodes || significantNodes.length === 0;
+    return significantNodes.length === 0;
 };
